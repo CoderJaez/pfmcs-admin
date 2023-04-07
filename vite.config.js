13ceb3bb90b5115cd59dd89a60edfbf1132e3ff0
JON REY L. LUMAYAG <jaezer.razor@gmail.com>
2023-04-08 01:49:59 +0800

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/smartpoultryfarm.cloud/",
+  base: "/pfmcs-admin/",
   plugins: [react()],
 });
